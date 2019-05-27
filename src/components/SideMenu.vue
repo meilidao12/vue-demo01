843025ad392353011bdc1c44731f09d05197619a
meilidao12 <330629898@qq.com>
2019-05-27 17:09:46 +0800

    123

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,18 +1,22 @@
 <template>
 <div>
     <ul>
-        <!-- <li><router-link>第一页</router-link></li> -->
+        <li><router-link to="demo1">第一页</router-link></li>
         <li><a href="#">第二页</a></li>
         <li><a href="#">第三页</a></li>
     </ul>
-    <!-- <router-view></router-view> -->
+    <router-view></router-view>
 </div>
 </template>
 
 
 <script>
+import demo from './demo'
+
 export default {
-    
+    components:{
+        demo1:demo
+    }
 }
 </script>
 
