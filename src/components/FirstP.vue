a4cd110a2730b83bd507fae158d25b2b452f29b1
meilidao12 <330629898@qq.com>
2019-05-28 14:12:23 +0800

    123

---
+++ src/components/FirstP.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>我是头部组件---{{title}}---{{homemsg}}</h2>
+        <button @click="run('123')">执行父组件的方法</button>
+        <br/>
+        <br/>
+        <button @click="getParent()">获取父组件的数据和方法</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg: '子组件的message'
+        }
+    },
+    methods:{
+        getParent(){
+            // alert("触发了getParent事件" + this.home.msg);
+            
+            this.home.run("234");
+        }
+    },
+    props:['title','homemsg','run','home']
+}
+</script>
+
+
--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,20 +1,36 @@
 <template>
 <div>
+    <v-firstp :title="title" :homemsg="msg" :run="run" :home="this"></v-firstp>
+    <br>
+    <hr>
     <ul>
         <li><a href="#">第一页</a></li>
         <li><a href="#">第二页</a></li>
         <li><a href="#">第三页</a></li>
     </ul>
+    <button v-on:click="run">点击触发</button>
     <router-view></router-view>
 </div>
 </template>
 
 
 <script>
-
-
+import FirstP from './FirstP';
 export default {
-
+    data(){
+        return{
+            msg:'我是一个home组件',
+            title:'首页111'
+        }
+    },
+    methods:{
+        run:function(data){
+            alert('我是home组件的run方法' + data);
+        }
+    },
+    components:{
+        'v-firstp':FirstP
+    }
 }
 </script>
 
